b13ed957c21ec2bac2b0312deae0ad023ca164f0
box <57877290+bboxlin@users.noreply.github.com>
2021-01-17 00:00:22 +0000

    Sync LeetCode submission - Add Two Numbers (java)

--- record/problems/add_two_numbers/solution.java
+++ record/problems/add_two_numbers/solution.java
@@ -9,24 +9,30 @@
  * }
  */
 class Solution {
-    
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode dummyHead = new ListNode(0);
-    ListNode p = l1, q = l2, curr = dummyHead;
-    int carry = 0;
-    while (p != null || q != null) {
-        int x = (p != null) ? p.val : 0;
-        int y = (q != null) ? q.val : 0;
-        int sum = carry + x + y;
-        carry = sum / 10;
-        curr.next = new ListNode(sum % 10);
-        curr = curr.next;
-        if (p != null) p = p.next;
-        if (q != null) q = q.next;
+        ListNode head = new ListNode(0);
+        ListNode sum = head;
+        int cin = 0;
+        while(l1 != null  || l2 != null){
+            int l1_val = (l1 == null) ? 0 : l1.val;
+            int l2_val = (l2 == null) ? 0 : l2.val;
+            
+            int cur_sum = l1_val + l2_val + cin;
+            cin = cur_sum / 10;
+            int last_digit = cur_sum % 10;
+            
+            ListNode new_node = new ListNode(last_digit);
+            sum.next = new_node;
+            
+            if(l1 != null) l1=l1.next;
+            if(l2 != null) l2=l2.next;
+            sum = sum.next;
+        } 
+        if(cin>0){
+            ListNode new_node = new ListNode(cin);
+            sum.next = new_node;
+        }
+        return head.next;
+         
     }
-    if (carry > 0) {
-        curr.next = new ListNode(carry);
-    }
-    return dummyHead.next;
-}
 }
