bf17f5278d42bac516334c89210b74b374bba82d
box <57877290+bboxlin@users.noreply.github.com>
2021-01-24 01:33:42 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (java)

--- record/problems/binary_tree_inorder_traversal/solution.java
+++ record/problems/binary_tree_inorder_traversal/solution.java
@@ -15,19 +15,27 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        //left rode right
-        Stack <TreeNode> stack = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-        while(!stack.empty() || root != null){
-            while(root != null){
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            list.add(root.val);
-            root = root.right;
-        }
+        //left node right
+        // Stack <TreeNode> stack = new Stack<>();
+        // List<Integer> list = new ArrayList<>();
+        // while(!stack.empty() || root != null){
+        //     while(root != null){
+        //         stack.push(root);
+        //         root = root.left;
+        //     }//当最左边的child == null 退出
+        //     root = stack.pop();  //root = 目前的node
+        //     list.add(root.val);  //加入目前的node;
+        //     root = root.right;   //root = 目前的右边child
+        // }
+        // return list;
+      List<Integer> list = new ArrayList(); 
+      return dfs(root, list);
+    }
+    public List<Integer> dfs (TreeNode root, List<Integer> list){
+        if(root == null) return list;
+        dfs(root.left, list);
+        list.add(root.val);
+        dfs(root.right, list);
         return list;
-            
     }
 }
