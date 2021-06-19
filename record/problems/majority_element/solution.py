a35d2d7f7b92e3455cfd35beaffcd384649f2815
box <57877290+bboxlin@users.noreply.github.com>
2021-06-19 23:21:46 +0000

    Sync LeetCode submission - Majority Element (python3)

--- record/problems/majority_element/solution.py
+++ record/problems/majority_element/solution.py
@@ -1,7 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        def helper(i,j):
+            if i == j: return nums[i]
+            mid = (i+j)//2
+            leftMajor = helper(i,mid)
+            rightMajor = helper(mid+1,j)
+            
+            if leftMajor == rightMajor: return leftMajor
+                
+            leftCount = sum(1 for i in range(i,j+1) if nums[i] == leftMajor)
+            rightCount = sum(1 for i in range(i,j+1) if nums[i] == rightMajor)
+            
+            return leftMajor if leftCount > rightCount else rightMajor
+        
+        return helper(0,len(nums)-1)
+            
+            
             
              
             
