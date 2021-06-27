e0933a19f2c792a12ed4a30b21eda9eeb7c4cd2d
box <57877290+bboxlin@users.noreply.github.com>
2021-06-27 04:24:11 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- record/problems/combination_sum/solution.py
+++ record/problems/combination_sum/solution.py
@@ -1,14 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[] for _ in range(target+1)]
-        for c in candidates:
-            for i in range(1,target+1):
-                if i < c: continue
-                if i == c: dp[i].append([c])
-                if i > c:
-                    for sublist in dp[i-c]:
-                        dp[i].append(sublist+[c])
-        return dp[target]
+        res = []
+        def dfs(i, one_solution, cur_value):       
+            if cur_value < 0 or i >= len(candidates): return
+            if cur_value == 0:
+                res.append(one_solution.copy())
+                return
+            one_solution.append(candidates[i])
+            dfs(i, one_solution, cur_value - candidates[i])
+            one_solution.pop()
+            dfs(i+1, one_solution, cur_value)
+        dfs(0,[],target)
+        return res
        
         
         
