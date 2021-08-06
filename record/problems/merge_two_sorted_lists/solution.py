1898e872a45daa53e8d0b8f148afb4b2d0cdb2cf
box <57877290+bboxlin@users.noreply.github.com>
2021-08-06 00:44:20 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

---
+++ record/problems/merge_two_sorted_lists/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 != None and l2 != None:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        
+        if l1 != None: cur.next = l1
+        if l2 != None: cur.next = l2
+        
+        return dummy.next
+                
+        
