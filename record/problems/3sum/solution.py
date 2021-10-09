d9dde1c53b4f180ad51a469ad3a8b61bf89d0ee1
box <57877290+bboxlin@users.noreply.github.com>
2021-10-09 01:04:23 +0000

    Sync LeetCode submission - 3Sum (python3)

---
+++ record/problems/3sum/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        if not nums or n<3: return []
+        nums.sort()
+        
+        for i in range(n):
+            if nums[i] > 0: return res 
+            if i > 0 and nums[i] == nums[i-1]: continue
+            l = i+1
+            r = n-1
+            while l<r:
+                if nums[i] + nums[l] + nums[r] == 0: 
+                    res.append([nums[i], nums[l], nums[r]])
+                    while (l<r and nums[l] == nums[l+1]): 
+                        l=l+1
+                    while (l<r and nums[r] == nums[r-1]):
+                        r=r-1
+                    l=l+1
+                    r=r-1
+                elif nums[i] + nums[l] + nums[r] > 0:r=r-1
+                else: l=l+1
+                    
+        return res
+                    
+                
