c4c976a97445bf2b7560c2e84e546b2eb73d0794
box <57877290+bboxlin@users.noreply.github.com>
2021-10-31 09:27:43 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- record/problems/sort_colors/solution.py
+++ record/problems/sort_colors/solution.py
@@ -1,28 +1,3 @@
-# class Solution:
-#     def sortColors(self, nums: List[int]) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         # [0,0,0,1,0,0,2,2,2]
-#         # [2,2,2,1,0,0,0,0,0]
-#         #  i         l
-#         #  s
-#         n = len(nums)
-#         s, l = 0, n-1
-#         i = 0
-#         while i <= l:
-#             while i <= l and nums[i] == 2:
-#                 nums[i], nums[l] = nums[l], nums[i]
-#                 l-=1
-                
-#             if nums[i] == 0:
-#                 nums[i], nums[s] = nums[s], nums[i]
-#                 s+=1 
-#             i+=1
-                
-    
-    
-#         #[2 1 2]
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         i, l = 0, 0
@@ -35,3 +10,6 @@ class Solution:
                 nums[i], nums[l] = nums[l], nums[i] #swap
                 i += 1
             l += 1
+            
+
+  
