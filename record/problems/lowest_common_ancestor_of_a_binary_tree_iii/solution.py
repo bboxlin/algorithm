2c1a8b00fbdf04322570f0c527491aebf268f2b0
box <57877290+bboxlin@users.noreply.github.com>
2021-11-12 09:24:01 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree III (python3)

---
+++ record/problems/lowest_common_ancestor_of_a_binary_tree_iii/solution.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        pPath = set()  
+        while p:
+            pPath.add(p) 
+            p = p.parent 
+            
+        while q:
+            if q in pPath:
+                return q
+            q = q.parent
+
+        return None
+            
