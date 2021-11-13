b3825ed10e1fa8c834d757e674f04a6b5a1bca41
box <57877290+bboxlin@users.noreply.github.com>
2021-11-13 11:06:56 +0000

    Sync LeetCode submission - Squares of a Sorted Array (python3)

---
+++ record/problems/squares_of_a_sorted_array/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        '''
+        two extreme are left and right
+        pick the max one and put it into the res list from last to first
+        '''
+        l, r = 0, len(nums)-1
+        res = [0]*len(nums)
+        for i in range(len(nums)-1, -1, -1):
+            if abs(nums[l]) < abs(nums[r]):
+                res[i] = nums[r] * nums[r]
+                r -= 1
+            else:
+                res[i] = nums[l] * nums[l]
+                l += 1
+        return res
