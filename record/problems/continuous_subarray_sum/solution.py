1f8799f45d0eddd349a0e0f142c60752f7eaf8be
box <57877290+bboxlin@users.noreply.github.com>
2021-11-22 06:05:20 +0000

    Sync LeetCode submission - Continuous Subarray Sum (python3)

--- record/problems/continuous_subarray_sum/solution.py
+++ record/problems/continuous_subarray_sum/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        presum = [0]*(len(nums)+1)
+        # rem : index
+        memo = {0 : -1}
         cursum = 0
-        for i in range(1, len(presum)):
-            presum[i] = nums[i-1]+cursum
-            cursum = presum[i]        
-        # [0, 1, 1]
-        # 
-        premod = set()
-        for i in range(2,len(presum)):
-            rj = presum[i] % k
-            ri = presum[i-2] % k
-            premod.add(ri)
-            if rj in premod: return True      
+        for i in range(len(nums)):
+            cursum += nums[i]
+            rem = cursum % k
+            if rem in memo.keys() and i - memo[rem] > 1 :
+                return True
+            
+            if rem not in memo.keys():
+                memo[rem] = i
         return False
-        
+    
+
