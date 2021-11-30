2a5d77c7c49c66316c506c84c82fa698490ccc87
box <57877290+bboxlin@users.noreply.github.com>
2021-11-30 07:20:36 +0000

    Sync LeetCode submission - Move Zeroes (python3)

--- record/problems/move_zeroes/solution.py
+++ record/problems/move_zeroes/solution.py
@@ -3,18 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # i = slow pointer, j = fast pointer
-        # i<=j, i+x=j, where among the x range, [i+x] = 0
-        # i++ for each swap when [j] != 0 is safe
         i = 0
         for j in range(len(nums)):
             if nums[j] != 0:
-                temp = nums[j]
-                nums[j] = nums[i]
-                nums[i] = temp
+                nums[i], nums[j] = nums[j], nums[i]
                 i+=1
                 
-             
-                
+
+        
             
                 
+        
