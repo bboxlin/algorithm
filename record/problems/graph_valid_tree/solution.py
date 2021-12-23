d8d12aaa2090063e42de89e6c38753d397095b53
box <57877290+bboxlin@users.noreply.github.com>
2021-12-23 07:18:45 +0000

    Sync LeetCode submission - Graph Valid Tree (python3)

---
+++ record/problems/graph_valid_tree/solution.py
@@ -0,0 +1,36 @@
+# UnionFind class
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return False
+        
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+            return True
+        
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1: return False
+        uf = UnionFind(n)
+        for x,y in edges:
+            if not uf.union(x,y):
+                return False
+        return True 
