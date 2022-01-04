489488a0d1ad8b3916e6ac5d3dedec47b77abc02
box <57877290+bboxlin@users.noreply.github.com>
2022-01-04 06:40:44 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (python3)

--- record/problems/kth_largest_element_in_an_array/solution.py
+++ record/problems/kth_largest_element_in_an_array/solution.py
@@ -1,29 +1,14 @@
+import heapq
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
-        def divide_conquer(arr):
-            if len(arr) > 1:
-                mid = len(arr) >> 1
-                l = divide_conquer(arr[:mid])
-                r = divide_conquer(arr[mid:])
-                combine(arr, l, r)
-            return arr
-        
-        def combine(arr, left, right):
-            i = 0 
-            while left and right:
-                if left[0] > right[0]:
-                    arr[i] = left.pop(0)
-                else:
-                    arr[i] = right.pop(0)
-                i += 1
-            while right:
-                arr[i] =right.pop(0)
-                i+=1
-            while left:
-                arr[i] = left.pop(0)
-                i+=1          
-        return divide_conquer(nums)[k-1]
+        maxheap = [-1*n for n in nums]
+        heapq.heapify(maxheap)
+        res = None
+        while k > 0:
+            res = heapq.heappop(maxheap) * (-1)
+            k -= 1
+        return res
         
             
         
