c1b463e26a0d0985618afbd1561db730c6f24512
box <57877290+bboxlin@users.noreply.github.com>
2022-01-06 07:10:15 +0000

    Sync LeetCode submission - Reverse String (python3)

--- record/problems/reverse_string/solution.py
+++ record/problems/reverse_string/solution.py
@@ -1,13 +1,8 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        
-        size = len(s)
-        half = size//2
-        for i in range(0, half):
-            temp = s[i]
-            s[i] = s[size-i-1]
-            s[size-i-1] = temp
-            
+        def reverse(l, r):
+            if l >= r:
+                return
+            s[l], s[r] = s[r], s[l]
+            reverse(l+1, r-1)
+        reverse(0, len(s)-1)
