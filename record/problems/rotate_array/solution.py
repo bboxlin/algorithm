9c95d35d47b1e5b284f856ef57ab73a3f0c6e667
box <57877290+bboxlin@users.noreply.github.com>
2022-01-25 08:49:29 +0000

    Sync LeetCode submission - Rotate Array (python3)

--- record/problems/rotate_array/solution.py
+++ record/problems/rotate_array/solution.py
@@ -1,16 +1,19 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        def reverseSub(arr, i, j):
+        
+        def reversing(i, j):
             while i < j:
-                temp = nums[i]
-                nums[i] = nums[j]
-                nums[j] = temp
-                j -= 1
-                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
+                i, j = i+1, j-1
+        
         k = k % len(nums)
-        nums.reverse()
-        reverseSub(nums, 0, k-1)
-        reverseSub(nums, k, len(nums)-1)
+        # reverse all
+        reversing(0, len(nums)-1)
+        
+        # reverse first k
+        reversing(0, k - 1)
+        
+        # reverse remaining
+        reversing(k, len(nums)-1)
+            
+        
