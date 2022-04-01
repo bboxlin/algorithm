fd40d9dfaa09f282a03393174437962319df2d67
box <57877290+bboxlin@users.noreply.github.com>
2022-04-01 02:29:49 +0000

    Sync LeetCode submission - Reverse String (python3)

--- record/problems/reverse_string/solution.py
+++ record/problems/reverse_string/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        def reverse(l, r):
-            if l >= r:
-                return
+        l, r = 0, len(s)-1
+        while l < r:
             s[l], s[r] = s[r], s[l]
-            reverse(l+1, r-1)
-        reverse(0, len(s)-1)
+            l += 1
+            r -= 1
+    
