ab35b26c945438ad1551998a53b04db65b4f27ce
box <57877290+bboxlin@users.noreply.github.com>
2022-04-19 07:14:27 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- record/problems/top_k_frequent_elements/solution.py
+++ record/problems/top_k_frequent_elements/solution.py
@@ -1,45 +1,28 @@
 import heapq
-class Solution:
-    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-    #     freq = {}
-    #     for n in nums:
-    #         if n not in freq:
-    #             freq[n] = 1
-    #         else:
-    #             freq[n] += 1
-    #     max_heap = [(-fre, n) for n, fre in freq.items()]
-    #     heapq.heapify(max_heap)
-    #     res = [heapq.heappop(max_heap)[1] for i in range(k)]
-    #     return res
 
+class Node:
+    def __init__(self, val, frequency):
+        self.val = val
+        self.frequency = frequency
+    
+    def __lt__(self, other):
+        if self.frequency > other.frequency:  
+            return True    
+        else:  
+            return False                      
+
+class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # N array slots
-        buckets = [[] for _ in range(len(nums) + 1)]
-        
-        # track down the frequency of each number
-        freq = {}
-        for n in nums:
-            if n not in freq:
-                freq[n] = 1
-            else:
-                freq[n] += 1
-        
-        # each slot with a list container, a number appends to corresponding frequency slot
-        for number, frequency in freq.items():
-            buckets[frequency].append(number)
-        
-        # O(k) in time here 
-        res = []
-        N = len(buckets) - 1 
-        for i in range(N, -1, -1):
-            if len(buckets[i]) > 0:
-                for val in buckets[i]:
-                    if len(res) >= k:
-                        return res
-                    res.append(val)
- 
-        return res 
-            
-            
+        freq = Counter(nums)
+        maxheap = [Node(val, f) for val, f in freq.items()]
+        heapq.heapify(maxheap)
+        ans = []
+        while k:
+            node = heapq.heappop(maxheap)
+            ans.append(node.val)
+            k -= 1
+        return ans
+
+
 
             
