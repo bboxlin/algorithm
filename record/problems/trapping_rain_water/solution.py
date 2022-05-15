103c794bc97b49dcee63a1bece9087f10841710a
box <57877290+bboxlin@users.noreply.github.com>
2022-05-15 06:15:31 +0000

    Sync LeetCode submission - Trapping Rain Water (python3)

--- record/problems/trapping_rain_water/solution.py
+++ record/problems/trapping_rain_water/solution.py
@@ -1,26 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        
-        res = 0
-        max_height = max(height)
-        max_h_index = height.index(max_height)
-        last_index = len(height) - 1
-        leftMaxHeight, rightMaxHeight = height[0], height[-1]
-        
-        # get waters from left side of max_h_index
-        for l in range(0, max_h_index):
-            if height[l] > leftMaxHeight:
-                leftMaxHeight = height[l]
+        l, r = 0, len(height) - 1
+        lMax, rMax = height[l], height[r]
+        acc = 0
+        while l < r:
+            if lMax < rMax:
+                l += 1
+                lMax = max(lMax, height[l])
+                acc += lMax - height[l]
             else:
-                res += leftMaxHeight - height[l]
-        
-        # get waters from right side of max_h_index
-        for r in range(last_index, max_h_index, -1):
-            if height[r] > rightMaxHeight:
-                rightMaxHeight = height[r]
-            else:
-                res += rightMaxHeight - height[r]
-                
-        return res
-                
-                
+                r -= 1
+                rMax = max(rMax, height[r])
+                acc += rMax - height[r]
+        return acc
