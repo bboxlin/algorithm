eea69c691c4429ef727beb7eb3813d892973f9ae
box <57877290+bboxlin@users.noreply.github.com>
2022-05-19 04:24:07 +0000

    Sync LeetCode submission - Same Tree (python3)

--- record/problems/same_tree/solution.py
+++ record/problems/same_tree/solution.py
@@ -6,31 +6,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    
-        def bfs(p,q):
-            que = collections.deque()
-            que.append(p)
-            que.append(q)
-            while que:
-                node1 = que.popleft()
-                node2 = que.popleft()
-                if node1 == None and node2 == None: continue
-                if node1 == None or node2 == None or node1.val != node2.val: return False
-                que.append(node1.left)
-                que.append(node2.left)
-                que.append(node1.right)
-                que.append(node2.right)
+        if not p and not q:
             return True
-        return bfs(p,q)
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        isLeftSame = self.isSameTree(p.left, q.left)
+        isRightSame = self.isSameTree(p.right, q.right)    
+        return isLeftSame and isRightSame
     
-#         def dfs(p,q):
-            
-#             if p == None and q == None: return True
-#             elif p == None or q == None: return False
-            
-#             if p.val == q.val:
-#                 return dfs(p.left, q.left) and dfs(p.right, q.right)
-#             else:
-#                 return False
-            
-#         return dfs(p,q)
