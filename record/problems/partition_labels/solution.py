62c1fdff4eb3a475eb7e4cbead0a14be9e81e30b
box <57877290+bboxlin@users.noreply.github.com>
2022-06-11 19:13:59 +0000

    Sync LeetCode submission - Partition Labels (python3)

--- record/problems/partition_labels/solution.py
+++ record/problems/partition_labels/solution.py
@@ -1,17 +1,25 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        cnt = Counter(s)
-        myset = {}
-        partition = []
-        result = []
-        for letter in s:
-            if letter not in myset:
-                myset[letter] = cnt.get(letter)
-            partition.append(letter)
-            myset[letter] -= 1
-            if sum(myset.values()) == 0:
-                result.append(len(partition))
+        # lastIdxMemo = { c:i for i, c in enumerate(s)}
+        # ans = []
+        # l, r = 0, 0
+        # for i, c in enumerate(s):
+        #     r = max(r, lastIdxMemo[c])
+        #     if i == r:
+        #         ans.append(r-l+1)
+        #         l = r + 1
+        # return ans
+        freq = collections.Counter(s)
+        cnter, partition = {}, []
+        ans = []
+        l = 0
+        for i, c in enumerate(s):
+            if c not in cnter.keys():
+                cnter[c] = freq[c]
+            cnter[c] -= 1
+            partition.append(c)
+            
+            if sum(cnter.values()) == 0:
+                ans.append(len(partition))
                 partition.clear()
-        return result
-
-
+        return ans
