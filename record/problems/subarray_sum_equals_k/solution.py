49dc0dd8e0c6c55c90c45eb29f21f46d078a5678
box <57877290+bboxlin@users.noreply.github.com>
2022-06-12 23:57:05 +0000

    Sync LeetCode submission - Subarray Sum Equals K (python3)

--- record/problems/subarray_sum_equals_k/solution.py
+++ record/problems/subarray_sum_equals_k/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # presum : freq
-        memo = {0 : 1}
-        cursum = 0
-        res = 0
+        freq = collections.defaultdict(int)
+        freq[0] = 1
+        cursum = 0 
+        res = 0 
+        
         for n in nums:
-            cursum += n
-            if cursum - k in memo.keys():
-                res += memo[cursum - k]
-       
-            if cursum not in memo:
-                memo[cursum] = 1
-            else:
-                memo[cursum] += 1
+            cursum += n 
+            if cursum - k in freq.keys():
+                res += freq[cursum - k]
+            freq[cursum] += 1
         return res
+    
