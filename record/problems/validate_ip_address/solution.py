5862b157f6bd78d94b87ea5787f0013ef8e7769d
box <57877290+bboxlin@users.noreply.github.com>
2022-06-20 19:40:14 +0000

    Sync LeetCode submission - Validate IP Address (python3)

---
+++ record/problems/validate_ip_address/solution.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validIPAddress(self, queryIP: str) -> str:
+        
+        def checkIpv4(lst):
+            for strnum in lst:
+                try:
+                    intnum = int(strnum)
+                    if intnum < 0 or intnum > 255 or str(intnum) != strnum:
+                        return 'Neither'
+                except:
+                    return 'Neither'
+            return 'IPv4'
+        
+        
+        def checkIpv6(lst):
+            content = '0123456789abcdefABCDEF'
+            for stritem in lst:
+                if len(stritem) < 1 or len(stritem) > 4:
+                    return 'Neither'
+                for c in stritem:
+                    if c not in content:
+                        return 'Neither'
+            return 'IPv6'
+        
+        
+        if queryIP.count('.') == 3:
+            lst = queryIP.split('.')
+            return checkIpv4(lst)
+        elif queryIP.count(':') == 7:
+            lst = queryIP.split(':')
+            return checkIpv6(lst)
+        else:
+            return "Neither"
