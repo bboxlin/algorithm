5a2e5a1d6484a8c5f2f8d7f4f39010865bbbf9ec
box <57877290+bboxlin@users.noreply.github.com>
2022-06-20 04:02:44 +0000

    Sync LeetCode submission - Permutations (python3)

---
+++ record/problems/permutations/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+        def dfs(acc):
+            if len(acc) == len(nums):
+                res.append(acc.copy())
+            for num in nums:
+                if num in set(acc): continue
+                acc.append(num)
+                dfs(acc)
+                acc.pop()                   
+        dfs([])
+        return res
