4769c317a4ea6b161219235980d8ec0c6f3a31c4
box <57877290+bboxlin@users.noreply.github.com>
2022-06-22 03:52:19 +0000

    Sync LeetCode submission - Decode Ways (python3)

---
+++ record/problems/decode_ways/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:   
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for i in range(1, n+1):
+            cur, prev = s[i-1], s[i-2]
+            if '1' <= cur <= '9':
+                dp[i] += dp[i-1] 
+            if i > 1 and (prev == '1' or prev  == '2' and cur in '0123456'):   
+                dp[i] += dp[i-2]
+        return dp[n]
+    
