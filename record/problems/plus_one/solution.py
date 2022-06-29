f5941d74fe74f9a29566176c10bc28b5c5d74917
box <57877290+bboxlin@users.noreply.github.com>
2022-06-29 14:48:57 +0000

    Sync LeetCode submission - Plus One (python3)

--- record/problems/plus_one/solution.py
+++ record/problems/plus_one/solution.py
@@ -1,11 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1, -1, -1):
-            digits[i] += 1
-            if digits[i] < 10: return digits
-            else: digits[i] %= 10
-        return [1] + digits
-            
+        strint = ''.join([str(n) for n in digits])
+        intnum = int(strint) + 1 
+        return [int(n) for n in str(intnum)]
+    
+     # for i in range(len(digits)-1, -1, -1):
+        #     digits[i] += 1
+        #     if digits[i] < 10: return digits
+        #     else: digits[i] %= 10
+        # return [1] + digits
  
 
             
