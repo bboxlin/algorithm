00799abefb1ec7ee8fc175dbf8d8625d2c402d89
box <57877290+bboxlin@users.noreply.github.com>
2022-07-01 16:59:52 +0000

    Sync LeetCode submission - Longest Palindrome (python3)

--- record/problems/longest_palindrome/solution.py
+++ record/problems/longest_palindrome/solution.py
@@ -2,18 +2,14 @@ import collections
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         freq = Counter(s)
-        ans = 0
-        for c, cnt in freq.items():
-            x = cnt // 2 * 2
-            ans += x
+        ans = 0 
+        for ch, cnt in freq.items():
+            if cnt % 2 == 0:
+                ans += cnt 
+            else:
+                ans += cnt - 1
+            
             if ans % 2 == 0 and cnt % 2 == 1:
-                ans += 1            
+                ans += 1
         return ans
     
-    # "abccccdd"
-    # a:1
-    # b:1
-    # d:2
-    # c:4
-    
-    #2+2*2+1 = 7
