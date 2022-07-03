1419306fc204b8e0f323bcb7e19950479d67fce7
box <57877290+bboxlin@users.noreply.github.com>
2022-07-03 18:29:58 +0000

    Sync LeetCode submission - First Bad Version (python3)

--- record/problems/first_bad_version/solution.py
+++ record/problems/first_bad_version/solution.py
@@ -8,10 +8,10 @@ class Solution:
         res = 0
         l, r = 1, n
         while l <= r:
-            mid = l + r >> 1
+            mid = l+r>>1
             if isBadVersion(mid):
-                res = mid
                 r = mid - 1
+                res = mid 
             else:
                 l = mid + 1
         return res
