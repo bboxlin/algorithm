0e9747103d7ffaae881070724f488eb1645d70fe
box <57877290+bboxlin@users.noreply.github.com>
2022-07-18 05:10:05 +0000

    Sync LeetCode submission - Two Sum (java)

--- record/problems/two_sum/solution.java
+++ record/problems/two_sum/solution.java
@@ -3,14 +3,16 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
     
         Map<Integer, Integer> myMap = new HashMap<>();
+        int [] ans = {-1, -1};
         for(int i = 0; i<nums.length; i++){
              int value = target - nums[i];
             if(myMap.containsKey(value)){
-                //notice get (Object key) meaning to get the value in here is the index.
-                return new int[] {i,myMap.get(value)};
+                ans[0] = i;
+                ans[1] = myMap.get(value);
+                return ans;
             }
             myMap.put(nums[i],i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return ans;
     }
 }
