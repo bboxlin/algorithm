f0c1ff70294793632da86d11031ba534eca4768d
box <57877290+bboxlin@users.noreply.github.com>
2022-07-18 23:15:02 +0000

    Sync LeetCode submission - Sum of Subarray Ranges (python3)

--- record/problems/sum_of_subarray_ranges/solution.py
+++ record/problems/sum_of_subarray_ranges/solution.py
@@ -1,12 +1,31 @@
 class Solution:
-    # [1, 2, 3]
-    # 2-1, 3-1, 3-2 = 1,2,1
     def subArrayRanges(self, nums: List[int]) -> int:
-        ans = 0
-        for i in range(len(nums)):
-            ma, mi = nums[i], nums[i]
-            for j in range(i, len(nums)):
-                ma = max(nums[j], ma)
-                mi = min(nums[j], mi)
-                ans += ma-mi
-        return ans
+         
+        inf = float('inf')
+        arr = [-inf] + nums + [-inf] 
+        res = 0
+       
+    
+        # min minus
+        incre_stack = []
+        for i, num in enumerate(arr):
+            # incoming num < stack top
+            while incre_stack and arr[incre_stack[-1]] > num:
+                localMin_idx = incre_stack.pop()
+                localMin = arr[localMin_idx] 
+                res -= localMin * (i - localMin_idx)* (localMin_idx - incre_stack[-1])
+            incre_stack.append(i)
+         
+         
+        # max plus 
+        arr = [inf] + nums + [inf] 
+        decre_stack = []
+        for i, num in enumerate(arr):
+            while decre_stack and arr[decre_stack[-1]] < num:
+                localMax_idx = decre_stack.pop()
+                localMax = arr[localMax_idx]
+                res += localMax * (i - localMax_idx) * (localMax_idx - decre_stack[-1])
+            decre_stack.append(i)
+ 
+            
+        return res
