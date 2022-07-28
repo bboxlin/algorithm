659a8a51f3e5305ec89483a2a494962051dc631f
box <57877290+bboxlin@users.noreply.github.com>
2022-07-28 23:25:51 +0000

    Sync LeetCode submission - LRU Cache (python3)

--- record/problems/lru_cache/solution.py
+++ record/problems/lru_cache/solution.py
@@ -1,20 +1,50 @@
+class Node:
+    def __init__(self, key=-1, val=-1):
+        self.key, self.val = key, val
+        self.next, self.prev = None, None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = collections.OrderedDict()
-
+        self.cache = collections.defaultdict(Node)
+        self.lru, self.mru = Node(), Node() 
+        self.lru.next, self.mru.prev = self.mru, self.lru
+    
+    def delete(self, node):
+        prev_node = node.prev 
+        next_node = node.next 
+        prev_node.next = next_node 
+        next_node.prev = prev_node
+        
+    
+    def insert(self, node):
+        prev_node = self.mru.prev
+        prev_node.next = node 
+        self.mru.prev = node 
+        node.prev = prev_node 
+        node.next = self.mru
+        
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        if key in self.cache.keys():
+            node = self.cache[key]
+            self.delete(node)
+            self.insert(node)
+            return node.val 
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        self.cache[key] = value
-        self.cache.move_to_end(key)
+        if key in self.cache.keys():
+            node = self.cache[key]
+            self.delete(node)
+        self.cache[key] = Node(key=key, val=value)
+        self.insert(self.cache[key])
+        
         if len(self.cache) > self.capacity:
-            self.cache.popitem(0)
+            lru_node = self.lru.next 
+            self.delete(lru_node)
+            del self.cache[lru_node.key]
+        
          
         
     
