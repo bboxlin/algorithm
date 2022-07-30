652a30df4f3336a41a13e20f31e053ef01156be1
box <57877290+bboxlin@users.noreply.github.com>
2022-07-30 18:22:26 +0000

    Sync LeetCode submission - Single Number (python3)

--- record/problems/single_number/solution.py
+++ record/problems/single_number/solution.py
@@ -1,15 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        freq = {}
+        ans = 0
         for n in nums:
-            if n in freq:
-                freq[n] += 1
-            else:
-                freq[n] = 1
-        
-        for n, count in freq.items():
-            if count == 1:
-                return n
-        return None
+            ans ^= n
+        return ans
     
