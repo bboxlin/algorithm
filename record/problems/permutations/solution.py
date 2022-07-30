eee59e0a8e11e1e73a475bc3ba3be8e653a117a5
box <57877290+bboxlin@users.noreply.github.com>
2022-07-30 22:59:37 +0000

    Sync LeetCode submission - Permutations (python3)

--- record/problems/permutations/solution.py
+++ record/problems/permutations/solution.py
@@ -1,14 +1,31 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-
-        res = []
+        # res = []
+        # def dfs(acc):
+        #     if len(acc) == len(nums):
+        #         res.append(acc.copy())
+        #     for num in nums:
+        #         if num in set(acc): continue
+        #         acc.append(num)
+        #         dfs(acc)
+        #         acc.pop()                   
+        # dfs([])
+        # return res
+        counter = Counter(nums)
         def dfs(acc):
-            if len(acc) == len(nums):
+            if len(acc) == n:
                 res.append(acc.copy())
-            for num in nums:
-                if num in set(acc): continue
-                acc.append(num)
-                dfs(acc)
-                acc.pop()                   
+                return 
+            for num in counter.keys():
+                if counter[num] > 0:
+                    acc.append(num)
+                    counter[num] -= 1
+                    
+                    dfs(acc)
+                    
+                    acc.pop()
+                    counter[num] += 1
+        n = len(nums)
+        res = []
         dfs([])
-        return res
+        return res    
