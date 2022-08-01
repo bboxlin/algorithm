51da51e2ed333236364634bfce909d64b53491cf
box <57877290+bboxlin@users.noreply.github.com>
2022-08-01 18:13:39 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- record/problems/longest_palindromic_substring/solution.py
+++ record/problems/longest_palindromic_substring/solution.py
@@ -1,23 +1,18 @@
-class Solution:   
-    def longestPalindrome(self, s: str) -> str:   
-        def getPalindrome(l, r):
-            while l >=0 and r < len(s) and s[l] == s[r]:
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        def getPan(l, r):
+            while l >= 0 and r < n and s[l] == s[r]:
                 l -= 1
                 r += 1
-            return (l+1, r-1)
-        start, end = 0, 0
-        for i in range(len(s)):
-            p1_start, p1_end = getPalindrome(i, i)
-            p2_start, p2_end = getPalindrome(i, i+1)
-            p1_len = p1_end - p1_start
-            p2_len = p2_end -p2_start 
-            if p1_len > end - start:
-                start = p1_start
-                end = p1_end
-            if p2_len > end - start:
-                start = p2_start
-                end = p2_end
-        return s[start: end+1]
-                
-                
-                
+            return s[l+1:r]
+
+        n = len(s)
+        ans = ""
+        for i in range(n):
+            p1 = getPan(i, i)
+            p2 = getPan(i, i+1)
+            ans = p1 if len(p1) > len(ans) else ans
+            ans = p2 if len(p2) > len(ans) else ans
+        return ans
+            
