be8aef7ddaf580ea31830b88520c72c76060b45c
box <57877290+bboxlin@users.noreply.github.com>
2022-08-02 03:07:56 +0000

    Sync LeetCode submission - Kth Smallest Element in a Sorted Matrix (python3)

--- record/problems/kth_smallest_element_in_a_sorted_matrix/solution.py
+++ record/problems/kth_smallest_element_in_a_sorted_matrix/solution.py
@@ -1,24 +1,42 @@
 import heapq
 class Solution:
+    
+    # Binary Search 
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        def countSmallerOrEqual(m):
+            i, j = n-1, 0
+            num = 0
+            while i >= 0 and j < n:
+                if matrix[i][j] <= m:
+                    num += (i + 1)
+                    j += 1
+                else:
+                    i -= 1
+            return num
         
-        minHeap = []
-        N = len(matrix)
-        for r in range(min(k,N)):
-            # tuple (item, row, col)
-            tup = (matrix[r][0], r, 0)
-            minHeap.append(tup)
+        # ower+(upper-lower)/2 和 upper-(upper-lower)/2 
+        # 0 + (9-0)/2 = 0 + 4 = 4 靠左
+        # 9 - (9-0)/2 = 9 - 4 = 5 靠右
+        # O(log （Max - Min) * O(n)
+        n = len(matrix)
+        lo, hi = matrix[0][0], matrix[-1][-1]
+        while lo<hi:
+            mid = lo + (hi-lo) // 2
+            if countSmallerOrEqual(mid) < k:
+                lo = mid+1
+            else:
+                hi = mid 
+        return lo
         
-        heapq.heapify(minHeap)
-        
-        res = None
-        for i in range(k):
-            
-            val, r, c = heapq.heappop(minHeap)
-            res = val
-            
-            if c < N-1:
-                heapq.heappush(minHeap, (matrix[r][c+1], r, c+1))
-            
-        return res
+
+    # Heap Solution
+    # def kthSmallestHeap(self, matrix: List[List[int]], k: int) -> int:
+    #     heap = [(row[0], i, 0) for i, row in enumerate(matrix)] #O(n)
+    #     heapq.heapify(heap)  #O(n)
+    #     ret = 0
+    #     for _ in range(k):   # k*logN
+    #         ret, i, j = heapq.heappop(heap)
+    #         if j+1 < len(matrix[0]):
+    #             heapq.heappush(heap, (matrix[i][j+1], i, j+1))
+    #     return ret
             
