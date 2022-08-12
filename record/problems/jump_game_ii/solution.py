ee4b2a83cf0e4654b790666ca1617aedf62e0905
box <57877290+bboxlin@users.noreply.github.com>
2022-08-12 06:00:11 +0000

    Sync LeetCode submission - Jump Game II (python3)

---
+++ record/problems/jump_game_ii/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [float('inf')] * n
+        for i in range(n):
+            if i == 0: dp[i] = 0
+            maxnxt = min(i + nums[i], n-1)
+ 
+            for j in range(i+1, maxnxt+1):
+                dp[j] = min(dp[j], dp[i] + 1)
+        return dp[n-1]
+    
