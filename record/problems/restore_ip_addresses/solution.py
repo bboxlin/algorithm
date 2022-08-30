a2ebe95c250c0c93d99c91356df3ae831d3433dd
box <57877290+bboxlin@users.noreply.github.com>
2022-08-30 04:10:42 +0000

    Sync LeetCode submission - Restore IP Addresses (python3)

---
+++ record/problems/restore_ip_addresses/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ans = [] 
+        if len(s) > 12: return ans
+        
+        def dfs(i, dot, ip):
+            if dot == 4 and i >= len(s):
+                ans.append(ip[:-1])
+                return 
+            if dot == 4:
+                return 
+            for j in range(i, min(i+3, len(s) )):
+                curval = int(s[i:j+1])
+                if curval <= 255 and (i == j or s[i] != '0'):
+                    dfs(j+1, dot+1, ip+str(curval)+'.')
+                    
+        dfs(0, 0, "")
+        return ans
+                
