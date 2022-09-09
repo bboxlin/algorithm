d0642c183eef01a72efadceda3d01ea9547479cb
box <57877290+bboxlin@users.noreply.github.com>
2022-09-09 01:16:03 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (python3)

--- record/problems/longest_repeating_character_replacement/solution.py
+++ record/problems/longest_repeating_character_replacement/solution.py
@@ -1,13 +1,17 @@
-import collections
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        freqTable = collections.defaultdict(int)
-        l, maxlen = 0, 0
-        for r in range(len(s)):
-            freqTable[s[r]] += 1
-            while (r - l + 1) - max(freqTable.values()) > k:
+        """
+        Sliding window, where the frequency in a window maintained in a freqTable
+        
+        """
+        freqTable = defaultdict(int)
+        ans = 0 
+        l = 0 
+        for r, ch in enumerate(s):
+            freqTable[ch] += 1
+            while r-l+1 - max(freqTable.values()) > k:
                 freqTable[s[l]] -= 1
-                l += 1
-            maxlen = max(maxlen, r - l + 1)
-        return maxlen
+                l+=1
+            ans = max(ans, r-l+1)
+        return ans
             
