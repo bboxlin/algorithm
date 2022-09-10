9605188ea37932ec452d483e4baac7d4881cd192
box <57877290+bboxlin@users.noreply.github.com>
2022-09-10 21:27:00 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

--- record/problems/contains_duplicate/solution.py
+++ record/problems/contains_duplicate/solution.py
@@ -1,7 +1,27 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        for i in range(1, len(nums)):
+        # O(n^2) time O(1) Space
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if nums[i] == nums[j]: return True 
+        # return False
+        
+        
+        # O(n) time O(n) Space
+        # hashset = set() 
+        # for num in nums:
+        #     if num in hashset:
+        #         return True 
+        #     hashset.add(num)
+        # return False
+        
+        nums.sort() 
+        n = len(nums)
+        for i in range(1, n):
             if nums[i] == nums[i-1]:
-                return True
+                return True 
         return False
+        
+        
+     
