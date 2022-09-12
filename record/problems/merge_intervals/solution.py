823056a12f9f544a7b08f5d8df64f04ac7d7479c
box <57877290+bboxlin@users.noreply.github.com>
2022-09-12 17:33:45 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- record/problems/merge_intervals/solution.py
+++ record/problems/merge_intervals/solution.py
@@ -1,23 +1,14 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
-        #  perhaps overlap occur in:
-        #  start <= end >= nextStart <= nextEnd
-        #  lower bound = min(start, nextStart)
-        #  upper bound = max(end, nextEnd)
-        
-        #  buf if starts are sorted
-        #  start <= end >= nextStart <= nextEnd, where start must <= nextStart
-        #  lower bound = start
-        #  upper bound = max(end, nextEnd)
-        
-        intervals.sort(key = lambda x : x[0]) # sort by lower bound 
-        res = [intervals[0]]
-        for nextStart, nextEnd in intervals[1:]:
-            end = res[-1][1]
-            if end >= nextStart:
-                res[-1][1] = max(end, nextEnd)
+        heapq.heapify(intervals)
+        ans = [] 
+        while intervals:
+            left, right = heapq.heappop(intervals)
+            if len(ans) == 0:
+                ans.append([left,right])
+            elif ans[-1][1] >= left:
+                ans[-1][1] = max(right, ans[-1][1])
             else:
-                res.append([nextStart, nextEnd])
-        return res
-            
+                ans.append([left,right])
+        return ans
+                
