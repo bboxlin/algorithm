3551eff8d0023b2b84cd1a25459f19d1b99e99c7
box <57877290+bboxlin@users.noreply.github.com>
2022-09-13 04:02:02 +0000

    Sync LeetCode submission - Meeting Rooms II (python3)

--- record/problems/meeting_rooms_ii/solution.py
+++ record/problems/meeting_rooms_ii/solution.py
@@ -3,18 +3,19 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
         rooms = 1
-        # make sure the interval are sorted in this way.
-        sorted_interval = sorted(sorted(intervals, key = lambda x:x[1]))
-        
+        sorted_interval = sorted(intervals)
         min_heap = [sorted_interval[0][1]]
-        
         for start, end in sorted_interval[1:]:
             last_end = min_heap[0]
             if start < last_end:
                 rooms += 1
+                heapq.heappush(min_heap, end)
             else:
+                # merge the meeting
                 heapq.heappop(min_heap)
-            heapq.heappush(min_heap, end)
+                heapq.heappush(min_heap, end)
+            
+        print(len(min_heap))
         return rooms
     
         # [[4,9],[4,17],[9,10]]
