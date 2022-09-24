2f0f5daa020f65116669162c4b65712c196c33a3
box <57877290+bboxlin@users.noreply.github.com>
2022-09-24 03:55:24 +0000

    Sync LeetCode submission - Reformat Date (python3)

--- record/problems/reformat_date/solution.py
+++ record/problems/reformat_date/solution.py
@@ -1,6 +1,25 @@
 class Solution:
     def reformatDate(self, date: str) -> str:
-        lst = date.split()
-        month = {"Jan":'01', "Feb":'02', "Mar":'03', "Apr":'04', "May":'05', "Jun":'06', "Jul":'07', "Aug":'08', "Sep":'09', "Oct":'10', "Nov":'11', "Dec":'12'}
-        day = lst[0][:2] if len(lst[0]) == 4 else ('0'+lst[0][:1])
-        return lst[2]+'-'+month[lst[1]]+'-'+ day
+        monthDic = {"Jan":'01', "Feb":'02', "Mar":'03', "Apr":'04', "May":'05', "Jun":'06', "Jul":'07', "Aug":'08', "Sep":'09', "Oct":'10', "Nov":'11', "Dec":'12'}
+        dataList = date.split()
+        ans = []
+        for i in range(len(dataList)-1, -1, -1):
+            
+            # year
+            if i == 2:
+                curyear = dataList[i]
+                ans.append(curyear)
+            # month
+            elif i == 1:
+                curmonth = dataList[i]
+                digitMonth = monthDic[curmonth]
+                ans.append(digitMonth)
+            # day
+            else:
+                curday = dataList[i]
+                curday = curday[:-2]
+                if len(curday) == 1:
+                    curday = '0' + curday
+                ans.append(curday)
+        return "-".join(ans)
+        
