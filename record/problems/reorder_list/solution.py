52c251d7e49080d24915619a5f3b6f7ebdc60820
box <57877290+bboxlin@users.noreply.github.com>
2022-09-24 06:22:15 +0000

    Sync LeetCode submission - Reorder List (python3)

--- record/problems/reorder_list/solution.py
+++ record/problems/reorder_list/solution.py
@@ -8,35 +8,41 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-         
-        cur = slow.next 
-        prev = slow.next = None
-        while cur:
-            nxtnode = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxtnode
         
-        first = head
-        second = prev
+        def reverseLinkedList(node):
+            prev = None
+            while node:
+                nxtnode = node.next
+                node.next = prev
+                prev = node
+                node = nxtnode
+            return prev
+        
+        def getMidNode(node):
+            slow, fast = head, head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+            
+  
+        midnode = getMidNode(head)
+        # Cut Link
+        head2 = midnode.next 
+        midnode.next = None 
+        
+        head2 = reverseLinkedList(head2)
 
-        while second:
-            fnxt = first.next
-            snxt = second.next
-            
-            first.next = second
-            second.next = fnxt
-            
-            first = fnxt
-            second = snxt
-            
-            
-            
-             
-            
-            
+        cur = ListNode(next=head)
+        first = head
+        second = head2
+        while first or second:
+            if first:
+                cur.next = first
+                first = first.next 
+                cur = cur.next 
+            if second:
+                cur.next = second
+                second = second.next 
+                cur = cur.next
             
