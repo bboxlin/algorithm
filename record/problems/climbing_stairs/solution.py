834a516876eeb30cbf12344f4abb3f20d65fdfb9
box <57877290+bboxlin@users.noreply.github.com>
2022-09-24 21:19:07 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- record/problems/climbing_stairs/solution.py
+++ record/problems/climbing_stairs/solution.py
@@ -1,10 +1,8 @@
 class Solution:
+    @cache
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n+1)
-        for i in range(n+1):
-            if i == 0: dp[i] = 0
-            elif i == 1: dp[i] = 1
-            elif i == 2: dp[i] = 2
-            else:
-                dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+        if n < 2:
+            return 1
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+    
+        
