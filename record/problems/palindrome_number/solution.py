6aac56fcc5c5e590aae7c24b677edd9872a01c76
box <57877290+bboxlin@users.noreply.github.com>
2022-10-01 07:04:50 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- record/problems/palindrome_number/solution.py
+++ record/problems/palindrome_number/solution.py
@@ -1,9 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0: return False
-        reverse, xvalue = 0, x
+        if x < 0: return False 
+        if x == 0: return True 
+        
+        digits = []
         while x:
-            rem = x % 10
-            reverse = reverse*10 + rem
-            x = x//10
-        return reverse == xvalue
+            d = x % 10
+            x = x // 10
+            digits.append(d)
+        
+        l, r = 0, len(digits) - 1
+        while l < r:
+            if digits[l] != digits[r]:
+                return False 
+            l += 1
+            r -= 1
+        return True
+        
