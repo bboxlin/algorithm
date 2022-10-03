7e7eeb8cb099ccd54d93a68c8a665c27e33c7471
box <57877290+bboxlin@users.noreply.github.com>
2022-10-03 04:26:10 +0000

    Sync LeetCode submission - Minimum Time to Make Rope Colorful (python3)

--- record/problems/minimum_time_to_make_rope_colorful/solution.py
+++ record/problems/minimum_time_to_make_rope_colorful/solution.py
@@ -1,14 +1,24 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        ans = 0
-        i  = 0 
-        while i < len(colors):
-            groupSum, maxSum = 0, 0
-            j = i
-            while j < len(colors) and colors[j] == colors[i]:
-                groupSum += neededTime[j]
-                maxSum = max(maxSum, neededTime[j])
+        # greedy, always remove with less time
+        # suppose there are k consecutive color, we will have to remove k-1 ballon
+        # to do that, we can keep the max time consuming ballon
+        n = len(colors)
+        i = 0 
+        ans = 0 
+        while i < n:
+            j = i+1
+            curCosts = neededTime[i]
+            curMaxCost = neededTime[i]
+            while j < n and colors[j] == colors[j-1]:
+                curCosts += neededTime[j]
+                curMaxCost = max(curMaxCost, neededTime[j])
                 j += 1
-            ans += groupSum - maxSum
+            ans += curCosts-curMaxCost
             i = j
         return ans
+            
+                
+            
+            
+                
