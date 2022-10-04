448e1a16d0326859c709263f21696b3dd17b6b36
box <57877290+bboxlin@users.noreply.github.com>
2022-10-04 02:55:51 +0000

    Sync LeetCode submission - Path Sum (java)

--- record/problems/path_sum/solution.java
+++ record/problems/path_sum/solution.java
@@ -15,15 +15,15 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null) return false;
-        
-        if(root.left == null && root.right == null){
-            return root.val == targetSum? true: false;
+        if (root == null){
+            return false;
         }
-        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
-    
+        int newsum = targetSum - root.val;
+        if (root.left == null && root.right == null){
+            return newsum == 0; 
+        }
+        boolean isLeftPathFound = hasPathSum(root.left, newsum);
+        boolean isRightPathFound = hasPathSum(root.right, newsum);
+        return isLeftPathFound || isRightPathFound;
     }
-    
-    
- 
 }
