fc382c97ea7d480015d91e01e93067591f517610
box <57877290+bboxlin@users.noreply.github.com>
2022-10-20 02:34:53 +0000

    Sync LeetCode submission - Integer to Roman (python3)

--- record/problems/integer_to_roman/solution.py
+++ record/problems/integer_to_roman/solution.py
@@ -1,15 +1,10 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        mapper = [('M',1000), ('CM',900), ('D',500), ('CD',400), ('C',100), ('XC', 90), 
-                  ('L',50), ('XL', 40), ('X',10), ('IX',9), ('V',5), ('IV',4), ('I',1)]
-        
-        ans = []
-        for cha, val in mapper:
-            if num < val: continue 
-            cnt = num // val
-            ans.append(cnt*cha)
-            num = num % (cnt*val)
-        return ''.join(ans)
-            
-        
-           
+        l = [('M',1000), ('CM', 900), ('D', 500), ('CD', 400), ('C', 100), ('XC', 90), ('L',50), ('XL', 40), ('X', 10), ('IX', 9), ('V',5), ('IV', 4),('I', 1)]
+        ans = ""
+        while num:
+            for letter, val in l:
+                cnt = num // val
+                ans += cnt * letter
+                num = num % val
+        return ans
