a8ebff4ccf8af2e88433093dd65ef5bb25f0853f
box <57877290+bboxlin@users.noreply.github.com>
2022-10-27 00:45:29 +0000

    Sync LeetCode submission - Image Overlap (python3)

---
+++ record/problems/image_overlap/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        A = [(i, j) for i, row in enumerate(img1) for j, item in enumerate(row) if item]
+        B = [(i, j) for i, row in enumerate(img2) for j, item in enumerate(row) if item]
+        count = collections.Counter((ax-bx, ay-by) for ax, ay in A for bx, by in B)
+        return max(count.values() or [0])  
