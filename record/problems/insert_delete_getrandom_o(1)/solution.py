4448092fd7958c8a327528724501bd20094cf9ca
box <57877290+bboxlin@users.noreply.github.com>
2022-11-16 06:19:17 +0000

    Sync LeetCode submission - Insert Delete GetRandom O(1) (python3)

--- record/problems/insert_delete_getrandom_o(1)/solution.py
+++ record/problems/insert_delete_getrandom_o(1)/solution.py
@@ -1,31 +1,37 @@
-import random
 class RandomizedSet:
 
     def __init__(self):
-        self.arr = []
-        self.lookup = {}
-
+        self.arr = [] 
+        self.map = {}
+        
     def insert(self, val: int) -> bool:
-        if val in self.lookup.keys(): return False 
-        self.lookup[val] = len(self.arr)
+        if val in self.map:
+            return False
         self.arr.append(val)
+        self.map[val] = len(self.arr)-1
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.lookup.keys(): return False
-        idx = self.lookup[val]
+        if val not in self.map:
+            return False
+        idx = self.map[val]
+         
         
         # update arr
         lastval = self.arr[-1]
         self.arr[idx] = lastval
-        self.arr.pop() 
+         
         
         # update lookup
-        self.lookup[lastval] = idx
-        del self.lookup[val]
+        self.map[lastval] = idx
+        
+        # we put del at last because we might ended up
+        # with 'update arr' part does not effective 
+        del self.map[val]
+        self.arr.pop() 
          
         return True
-
+    
     def getRandom(self) -> int:
         return random.choice(self.arr)
 
