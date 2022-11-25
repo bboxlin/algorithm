3ab5ae8e8be528f8863580ad9831741f738b01e3
box <57877290+bboxlin@users.noreply.github.com>
2022-11-25 08:16:00 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (python3)

--- record/problems/shortest_path_in_binary_matrix/solution.py
+++ record/problems/shortest_path_in_binary_matrix/solution.py
@@ -1,35 +1,30 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        
-        if grid[0][0] != 0:
-            return -1
-        
         rows = len(grid)
         cols = len(grid[0])
         
-        direc = [(-1,-1), (1, 1), (1,-1), (-1,1), (0, 1), (1, 0), (0, -1), (-1, 0)]
+        if grid[0][0] != 0:
+            return -1 
         
-        visited = set()
-        q = deque([(0,0,1)])
-        visited.add((0,0))
-        
-        
+        q = deque([ (0,0) ])
+        ans = 1
+        grid[0][0] = 1
         while q:
-            i, j, d = q.popleft()      
-            
-            # since BFS first encounter is shortest!
-            if i == rows - 1 and j == cols - 1:
-                return d
-            
-            for di, dj in direc:
-                if 0 <= i + di < rows and 0 <= j + dj < cols:
-                    newi = i + di
-                    newj = j + dj
-                     
-                    if grid[newi][newj] == 0 and (newi, newj) not in visited:                  
-                        q.append((newi,newj,d+1)) 
-                    
-                    # mark current pos visited
-                    visited.add((newi,newj))      
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                if i == rows - 1 and j == cols - 1:
+                    return ans 
+                 
+                for di, dj in [(1,0),(-1,0),(0,1),(0,-1), (1,-1), (-1,1), (1,1), (-1,-1)]:
+                    if 0 <= i + di < rows and 0 <= j + dj < cols and grid[i+di][j+dj] == 0:
+                        # we want to mark visited here immediately 
+                        grid[i+di][j+dj] = 1
+                        q.append((i+di,j+dj))
+            ans += 1
         return -1
-  
+
+    """
+    1 0 0 
+    1 1 0 
+    1 1 0 
+    """
