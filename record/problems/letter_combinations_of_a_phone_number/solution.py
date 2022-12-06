dad5ee8656a9a1c3c806929c0df8b0af3548a4cc
box <57877290+bboxlin@users.noreply.github.com>
2022-12-06 23:58:19 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- record/problems/letter_combinations_of_a_phone_number/solution.py
+++ record/problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,27 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits: return []
-        memo = {'2':'abc',
-                '3':'def',
-                '4':'ghi',
-                '5':'jkl',
-                '6':'mno',
-                '7':'pqrs',
-                '8':'tuv',
-                '9':'wxyz'}
-        res = []
-        def dfs(combination,digits):
-            if not digits:
-                res.append(combination)
-            else:
-                curDigit = digits[0]
-                for letter in memo[curDigit]:
-                    dfs(combination+letter, digits[1:])   
 
-        dfs('',digits)
+        def dfs(i, acc):
+            if i == len(digits):
+                res.append(acc)
+                return 
+            d = digits[i]
+            for cha in graph[d]:
+                dfs(i+1, acc+cha)
+            
+
+
+        if not digits:
+            return []
+        graph = {'2':'abc',
+                 '3':'def',
+                 '4':'ghi',
+                 '5':'jkl',
+                 '6':'mno',
+                 '7':'pqrs',
+                 '8':'tuv',
+                 '9':'wxyz'}
+        res = []
+        dfs(0, "")
         return res
-                
-            
-        
-            
-            
