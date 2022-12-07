2e09ebaa2b8a1ef90b1be25f92948ac3f4ad9ca8
box <57877290+bboxlin@users.noreply.github.com>
2022-12-07 06:02:30 +0000

    Sync LeetCode submission - Spiral Matrix (python3)

--- record/problems/spiral_matrix/solution.py
+++ record/problems/spiral_matrix/solution.py
@@ -1,21 +1,30 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]: 
-        res = []
-        ROW, COL = len(matrix), len(matrix[0])
-        direc = {0:[0,1], 1:[1,0], 2:[0, -1], 3:[-1, 0]}
-        d = 0
-        r, c, dr, dc = 0, 0, 0, 1
-        for _ in range(ROW*COL):
-            res.append(matrix[r][c])
-            matrix[r][c] = 888
-            # check for the next position, if in valid, adjust dr and dc
-            if r+dr >= ROW or c+dc >= COL or matrix[r+dr][c+dc] == 888:
-                d = (d+1)%4
-                dr, dc = direc[d]
-            r += dr 
-            c += dc
-        return res
-                
-            
-            
-                
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ans = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+        while left < right and top < bottom:
+            for j in range(left, right):
+                ans.append(matrix[top][j])
+            top += 1
+
+            for i in range(top, bottom):
+                ans.append(matrix[i][right-1])
+            right -= 1
+
+            if left >= right or top >= bottom:
+                break
+
+            for j in range(right-1, left - 1, -1):
+                ans.append(matrix[bottom-1][j])
+            bottom -= 1
+
+            for i in range(bottom-1, top-1, -1):
+                ans.append(matrix[i][left])
+            left += 1
+        return ans
+
+
+
+
+        return ans
