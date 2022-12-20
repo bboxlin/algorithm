0228fed2d2f0f37baf45b862486011a3cc9b45fd
box <57877290+bboxlin@users.noreply.github.com>
2022-12-20 09:42:19 +0000

    Sync LeetCode submission - Maximum Product Subarray (python3)

--- record/problems/maximum_product_subarray/solution.py
+++ record/problems/maximum_product_subarray/solution.py
@@ -1,22 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        # the idea is to keep max and min dp as well, since we have negative number.
         n = len(nums)
         maxdp, mindp = [0]*n, [0]*n
         for i in range(n):
-            if i == 0: 
+            if i == 0:
                 maxdp[i] = nums[i]
-                mindp[i] = nums[i] 
+                mindp[i] = nums[i]
             else:
-                maxdp[i] = max(maxdp[i-1] * nums[i], mindp[i-1] * nums[i], nums[i])
-                mindp[i] = min(maxdp[i-1] * nums[i], mindp[i-1] * nums[i], nums[i])
-                
-        return max(maxdp)
+                maxdp[i] = max(maxdp[i-1]*nums[i], mindp[i-1]*nums[i], nums[i])
+                mindp[i] = min(maxdp[i-1]*nums[i], mindp[i-1]*nums[i], nums[i])
 
-    # def maxProduct(self, nums: List[int]) -> int:
-    #     ans = max(nums)
-    #     maxprod, minprod = 1, 1
-    #     for n in nums:
-    #         maxprod, minprod = max(maxprod * n, minprod * n, n), min(maxprod * n, minprod * n, n)
-    #         ans = max(maxprod, ans)
-    #     return ans
-       
+        return max(maxdp)
