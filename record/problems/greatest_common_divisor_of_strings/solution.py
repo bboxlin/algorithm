28b8b43191f2209a6991ac8a296db04ab44c4fda
box <57877290+bboxlin@users.noreply.github.com>
2023-02-01 06:16:43 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (python3)

--- record/problems/greatest_common_divisor_of_strings/solution.py
+++ record/problems/greatest_common_divisor_of_strings/solution.py
@@ -1,11 +1,16 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1+str2 != str2+str1:
-            return ""
-        elif str1 == str2:
-            return str1
-        elif len(str1) > len(str2):
-            return self.gcdOfStrings(str1[len(str2):], str2)
-        else:
-            return self.gcdOfStrings(str1, str2[len(str1):])
-            
+        ans = ""
+        n = min( len(str1), len(str2) )
+        for i in range(n):
+            p1 = s1 = str1[:i+1]
+            p2 = s2 = str2[:i+1]
+            if s1 != s2: continue 
+            while len(p1) < len(str1):
+                p1 += s1
+            while len(p2) < len(str2):
+                p2 += s2 
+            if p1 != str1 or p2 != str2: continue 
+            if len(s1) > len(ans):
+                ans = s1
+        return ans
