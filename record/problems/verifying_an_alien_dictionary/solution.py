6a56c941b2c067d2e5be60a6d5fa36f058adadb1
box <57877290+bboxlin@users.noreply.github.com>
2023-02-02 04:04:08 +0000

    Sync LeetCode submission - Verifying an Alien Dictionary (python3)

--- record/problems/verifying_an_alien_dictionary/solution.py
+++ record/problems/verifying_an_alien_dictionary/solution.py
@@ -1,35 +1,25 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        
-        def isGreater(second, first):
-            i, j = 0, 0
-            while i < len(first) and j < len(second):
-                if memo[first[i]] > memo[second[j]]:
+        lookup = {}
+        for i, c in enumerate(order):
+            lookup[c] = i
+            
+        n = len(words)
+        for i in range(1, n):
+            cur = words[i]
+            prv = words[i-1]
+
+            brk = False
+            m = min( len(cur), len(prv) )
+            for j in range(m):
+                if lookup[cur[j]] == lookup[prv[j]]:
+                    continue
+                if lookup[cur[j]] > lookup[prv[j]]:
+                    brk = True
+                    break
+                if lookup[cur[j]] < lookup[prv[j]]:
                     return False
-                elif memo[first[i]] < memo[second[j]]:
-                    return True
-                i += 1
-                j += 1
-            
-            # 1. i have some char left for traversal
-            if i < len(first): return False
-            
-            # 2. j some some char left for traversal 
-            elif j < len(second): return True
-            
-            # 3. in case both first and second equal all characters
-            return True
-       
-        if len(words) == 1: return True
-        memo = {}
-        for index, c in enumerate(order):
-            memo[c] = index
-        
-        for i in range(1, len(words)): # N for total N words in List
-            second = words[i]
-            first = words[i-1]
-            if isGreater(second, first):  # M character in a sublist       #Time: O(NM)
-                continue
-            else:
-                return False  
+            # cur >= prv then 
+            if not brk and len(prv) > len(cur):
+                return False
         return True
