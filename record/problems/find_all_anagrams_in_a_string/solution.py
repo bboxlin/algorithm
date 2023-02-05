721ed3f0da54bb86dfc823c06dbbbae7f748274d
box <57877290+bboxlin@users.noreply.github.com>
2023-02-05 00:40:52 +0000

    Sync LeetCode submission - Find All Anagrams in a String (python3)

--- record/problems/find_all_anagrams_in_a_string/solution.py
+++ record/problems/find_all_anagrams_in_a_string/solution.py
@@ -1,28 +1,19 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        lenS, lenP = len(s), len(p)
-        output = []
-        if lenS < lenP:
-            return output 
-        # s = "cbaebabacd", p = "abc"
-        #       | |
-        arr1, arr2 = [0]*26, [0]*26
-        for i in range(lenP):
-            idx1 = ord(p[i]) - ord('a')
-            idx2 = ord(s[i]) - ord('a')
-            arr1[idx1] += 1
-            arr2[idx2] += 1
-            
-        if arr1 == arr2: 
-            output.append(0)
+        pcnt = [0]*26
+        for c in p:
+            pcnt[ord(c) - ord('a')] += 1
         
-        for rightPos in range(lenP, lenS):
-            leftPos = rightPos - lenP 
-            arr2[ord(s[leftPos]) - ord('a')] -= 1
-            arr2[ord(s[rightPos]) - ord('a')] += 1
-           
-            if arr1 == arr2:
-                output.append(leftPos + 1)
-        
-        return output
-                
+        window = [0]*26
+        res = []
+        for i, c in enumerate(s):
+            if i < len(p):
+                window[ord(c) - ord('a')] += 1
+            else:
+                window[ord(c) - ord('a')] += 1
+                window[ ord(s[i-len(p)]) - ord('a')] -=1 
+            if window == pcnt:
+                res.append(i-len(p)+1)
+        return res
+
+
