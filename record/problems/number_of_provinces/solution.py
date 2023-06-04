70c828f196a94336051581c70abc1e4701784916
box <57877290+bboxlin@users.noreply.github.com>
2023-06-04 01:06:35 +0000

    Sync LeetCode submission - Number of Provinces (python3)

--- record/problems/number_of_provinces/solution.py
+++ record/problems/number_of_provinces/solution.py
@@ -1,38 +1,25 @@
-class UandF:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.rank = [1]*size
-        self.count = size
-    
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-    
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        if root_x != root_y:
-            if self.rank[root_x] < self.rank[root_y]:
-                self.root[root_x] = root_y
-            elif self.rank[root_x] > self.rank[root_y]:
-                self.root[root_y] = root_x
-            else:
-                self.root[root_y] = root_x
-                self.rank[root_x] += 1
-            self.count -= 1
-    
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:   
-        n = len(isConnected)
-        if n == 0: return 0
-        uf = UandF(n)
-        for row in range(n):
-            for col in range(row + 1, n):
-                if isConnected[row][col] == 1:
-                    uf.union(row, col)
-        print(uf.root)
-        return uf.count
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
 
-                    
+        def dfs(u):
+            visited.add(u)
+            for v in graph[u]:
+                if v not in visited:
+                    dfs(v)
+
+
+        graph = defaultdict(list)
+        n = len(isConnected)
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    graph[i].append(j)
+                    graph[j].append(i)
+
+        visited = set()
+        res = 0
+        for city in range(n):
+            if city not in visited:
+                dfs(city)
+                res += 1
+        return res
