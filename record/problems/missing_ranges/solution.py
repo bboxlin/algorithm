aa3caf90a8ef6b68e5f1564c733f9dcee9ce8186
box <57877290+bboxlin@users.noreply.github.com>
2023-06-16 23:23:01 +0000

    Sync LeetCode submission - Missing Ranges (python3)

--- record/problems/missing_ranges/solution.py
+++ record/problems/missing_ranges/solution.py
@@ -1,20 +1,15 @@
 class Solution:
-    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
         res = []
-        for n in nums:
-            if n > lower:
-                if n - lower == 1:
-                    res.append(str(lower))
-                else:
-                    res.append(str(lower) + "->" + str(n-1))
-            
-            lower = n + 1
-        
-        if lower < upper:
-            res.append(str(lower) + "->" + str(upper))
-        elif lower == upper:
-            res.append(str(lower))
-        
+        if len(nums) == 0: 
+            return [[lower, upper]]
+        if nums[0] - lower >= 1:
+            res.append([lower, nums[0]-1])
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i-1] > 1:
+                high = nums[i] - 1
+                low  = nums[i-1] + 1
+                res.append([low, high])
+        if upper - nums[-1] >= 1:
+            res.append([nums[-1]+1, upper]) 
         return res
-                    
-            
